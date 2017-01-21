0ae1c58d742d649a16188327458cc23cf3ecfddb
yastrouskaya <yastrouskaya@exadel.com>
2017-01-21 10:58:11 +0300

    added bootstrap and jquery, reformatted

--- app/js/services.js
+++ app/js/services.js
@@ -1,30 +1,30 @@
 'use strict';
 
 angular.module('F1FeederApp.services', [])
-    .factory('ergastAPIservice', function($http) {
+	.factory('ergastAPIservice', function ($http) {
 
-        var ergastAPI = {};
+		var ergastAPI = {};
 
-        ergastAPI.getDrivers = function() {
-            return $http({
-                method: 'JSONP',
-                url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
-            });
-        };
+		ergastAPI.getDrivers = function () {
+			return $http({
+				method: 'JSONP',
+				url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+			});
+		};
 
-        ergastAPI.getDriverDetails = function(id) {
-            return $http({
-                method: 'JSONP',
-                url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/driverStandings.json?callback=JSON_CALLBACK'
-            });
-        };
+		ergastAPI.getDriverDetails = function (id) {
+			return $http({
+				method: 'JSONP',
+				url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/driverStandings.json?callback=JSON_CALLBACK'
+			});
+		};
 
-        ergastAPI.getDriverRaces = function(id) {
-            return $http({
-                method: 'JSONP',
-                url: 'http://ergast.com/api/f1/2013/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
-            });
-        };
+		ergastAPI.getDriverRaces = function (id) {
+			return $http({
+				method: 'JSONP',
+				url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/results.json?callback=JSON_CALLBACK'
+			});
+		};
 
-        return ergastAPI;
-    });
+		return ergastAPI;
+	});
